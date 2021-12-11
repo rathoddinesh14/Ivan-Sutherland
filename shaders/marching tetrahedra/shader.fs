b6ca1ced37d7d80b9e2b20ec6c70672e4367c28c
Ankit <32204544+AnkitAI07@users.noreply.github.com>
2021-12-11 12:38:50 +0530

    Update shader.fs
    Colormap Added

--- shaders/marching tetrahedra/shader.fs
+++ shaders/marching tetrahedra/shader.fs
@@ -11,6 +11,41 @@
 // uniform vec3 lightSrc;
 // uniform vec3 camPos;
 
+in float z;
+
+vec4 colormap(float z){
+	
+	if(z < -0.8)
+	return vec4(0.168872, 0.250754, 0.015434, 1.0);
+
+	else if(z < -0.6)
+	return vec4(0.219751, 0.160845, 0.050890, 1.0);
+
+	else if(z < -0.4)
+	return vec4(0.309161,0.188452,0.115882,1.0);
+
+	else if(z < -0.2)
+	return vec4(0.388874,0.205646,0.168464,1.0);
+
+	else if(z < 0.0)
+	return vec4(0.701541,0.321894,0.286795,1.0);
+
+	else if(z < 0.2)
+	return vec4(0.790147,0.352941,0.362754,1.0);
+
+	else if(z < 0.4)
+	return vec4(0.878751,0.411952,0.458781,1.0);
+
+	else if(z < 0.6)
+	return vec4(0.901965,0.515864,0.584845,1.0);
+
+	else if(z < 0.8)
+	return vec4(0.921487,0.557268,0.696742,1.0);
+
+	else if(z <= 1)
+	return vec4(0.969001,0.719921,0.829062,1.0);
+}
+
 void main()
 {
 //     vec3 lightColor = vec3(1.0, 1.0, 1.0);
@@ -33,6 +68,7 @@ void main()
 // // --------------------------------------------------------
 
 
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = colormap(z);
     // gl_FragColor 	= vec4(vec3(1-vPosition.z/6) * (ambCol+diffCol+specCol) , 1.0);
 }
